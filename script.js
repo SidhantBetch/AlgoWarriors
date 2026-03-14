d056900ca7e31883f1d5dba8a03adba13fa15031
SidhantBetch <sidhantbtech@gmail.com>
2026-03-15 01:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startSimulation(){
+    window.location.href = "register.html";
+}
+
+function howItWorks(){
+    alert("Here you will explain how AlgoWarrior works.");
+}
+
+function logout(){
+    alert("Logged out successfully!");
+}
